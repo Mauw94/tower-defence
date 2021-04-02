0a475d4449911eb5df214e57d0e4b175706ee292
Maurits Seelen <mauritsseelen@gmail.com>
2021-04-02 16:21:43 +0200

    basic skeleton moves across the screen

--- arpg/Entities/Enemies/BasicSkeleton.cs
+++ arpg/Entities/Enemies/BasicSkeleton.cs
@@ -11,11 +11,17 @@ namespace towerdef.Entities.Enemies
         public BasicSkeleton(Texture2D texture) : base(texture)
         {
             HealthPoints = 100;
+            LinearVelocity = 0.5f;
             Position = new Vector2(Game1.ScreenWidth - 150, Game1.ScreenHeight / 2);
         }
 
         public override void Update(GameTime gameTime)
         {
+            var goTo = new Vector2(0, Game1.ScreenHeight / 2);
+            Vector2 direction = goTo - Position;
+            direction.Normalize();
+            Position += direction * LinearVelocity;
+
             base.Update(gameTime);
         }
 
